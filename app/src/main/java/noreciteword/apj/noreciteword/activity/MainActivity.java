67e8b88030a54a50a6e5afc92984df025e099562
vplanMan <1471782566@qq.com>
2016-11-20 20:18:04 +0800

    增加了ViewpagerActivity

--- app/src/main/java/noreciteword/apj/noreciteword/activity/MainActivity.java
+++ app/src/main/java/noreciteword/apj/noreciteword/activity/MainActivity.java
@@ -1,20 +1,23 @@
 package noreciteword.apj.noreciteword.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import noreciteword.apj.noreciteword.R;
 import noreciteword.apj.noreciteword.view.PanelSwitcher;
 
 public class MainActivity extends AppCompatActivity {
-
+    //private TextView la;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         PanelSwitcher mPanelSeitcher = (PanelSwitcher) findViewById(R.id.panelswitch);
 //        mPanelSeitcher.setLongClickable(true);
@@ -22,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
         View mSecondLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.behind, null);
         mPanelSeitcher.setAboveView(mFirstLayout);
         mPanelSeitcher.setBehindView(mSecondLayout);
+
+/*        la = (TextView) findViewById(R.id.la);
+        la.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ViewPagerActivity.class));
+            }
+        });*/
+
     }
 }
